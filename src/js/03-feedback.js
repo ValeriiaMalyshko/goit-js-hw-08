88a5e7a82cd22bda3c5ba3c9c9d9eadf9c5aefd1
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-02-19 19:49:36 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,8 @@
-
+import throttle from 'lodash.throttle';
+const form = document.querySelector('feedback-form');
+const mail = document.querySelector('name="email"');
+const mesagge = document.querySelector('name="message"')
+const savedData = localStorage.getItem('feedback-form-state');
+const parsedSavedData = JSON.parse(savedData);
+form.addEventListener('input', throttle(handleInput, 500));
+form.addEventListener('submit', handleSubmit);
