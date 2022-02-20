f7759a85c514ab17b96a7db6f58f0304d9691cdb
ValeriiaMalyshko <91088527+ValeriiaMalyshko@users.noreply.github.com>
2022-02-20 19:43:00 +0200

    change task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,38 @@
 import throttle from 'lodash.throttle';
-const form = document.querySelector('feedback-form');
-const mail = document.querySelector('name="email"');
-const mesagge = document.querySelector('name="message"')
-const savedData = localStorage.getItem('feedback-form-state');
-const parsedSavedData = JSON.parse(savedData);
+
+const form = document.querySelector('.feedback-form');
+
 form.addEventListener('input', throttle(handleInput, 500));
-form.addEventListener('submit', handleSubmit);
+
+initForm ();
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+const formData = new FormData(form);
+formData.forEach ((key, value) => console.log (value, key));
+event.currentTarget.reset();
+localStorage.removeItem('feedback-form-state'); 
+});
+
+function handleInput(event) {
+let savedData = localStorage.getItem('feedback-form-state');
+savedData = savedData ? JSON.parse(savedData) : {};
+savedData[event.target.name]=event.target.value;
+
+if(savedData) {
+    localStorage.setItem('feedback-form-state',JSON.stringify(savedData));
+}
+};
+
+function initForm() {
+    let savedData = localStorage.getItem('feedback-form-state');
+
+    if(!savedData){
+        return ;
+    }
+    else  {
+        savedData = JSON.parse(savedData);
+    }
+
+    Object.entries(savedData).forEach(([name, value]) => (form.elements[name].value = value),);
+}
